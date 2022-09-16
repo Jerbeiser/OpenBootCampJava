6084b7c2d440a84b197b462b640477ec38f34af8
Jorge E <85731789+Jerbeiser@users.noreply.github.com>
2022-09-16 13:43:37 +0200

    primer commit

---
+++ ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package paquete;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int entero = 1;
+        long tipoLong =1234l;
+        double tipoDouble = 2.3;
+        boolean tipoBoolean = true;
+        String cadena = "Es una cadena";
+        System.out.println("El tipo entero es "+ entero);
+        System.out.println("El tipo long es "+ tipoLong);
+        System.out.println("El tipo double es "+ tipoDouble);
+        System.out.println("El tipo boolean es "+ tipoBoolean);
+        System.out.println("El tipo entero cadena "+ cadena);
+    }
+}
